9b14f651c29a4ff2959a314fa1a2447f852e6b33
Tomas Morton <tomas.morton@hotmail.co.nz>
2022-07-21 10:13:01 -0400

    Add invalid solution for 1.8

---
+++ 1-8_ZeroMatrix.cs
@@ -0,0 +1,67 @@
+/*
+    Assumption:
+        This shouldn't automatically recurse otherwise the whole matrix becomes 0
+        0 < m,n
+    Ideas:
+        iterate on each cell, and if there is a row/col with 0 then set it to 0
+            use a hashmap/memo to prevent repeat checks
+            
+        iterate on each cell, and if it is 0 then mark the related cells to be 0d.
+            Update at the end based on marks.
+            
+        iterate on 0,0; 1,1; 2,2; and check
+            Implemented below, but this doesn't actually work
+*/
+public class ZeroMatrix //Note: Doesn't solve the problem!
+{
+    private int[][] _matrix;
+    private int _rows;
+    private int _cols;
+    
+    public void Initialise(int[][] matrix)
+    {
+        if (matrix == null) return null;
+        if (matrix.Length == 0 || matrix[0].Length ==) return matrix; //Assumption that there will be at least one row and col
+        
+        _matrix = matrix;
+        _rows = matrix.Length;
+        _cols = matrix[0];
+        int iterations = Math.Min(_rows, _cols);
+        
+        for (i = 0; i < iterations; i++)
+        {
+            if (ShouldBeZeroed(i))
+                ZeroMatrixSegment(i);
+        }
+        
+        return matrix;
+    }
+    
+    private bool ShouldBeZeroed(int segment)
+    {
+        for (var row = segment; row < _rows; row++)
+        {
+            if (_matrix[row, segment] == 0)
+                return true;
+        }
+        for (var col = segment; col < _cols; col++)
+        {
+            if (_matrix[segment, col] == 0)
+                return true;
+        }
+        
+        return false;
+    }
+    
+    private void ZeroMatrixSegment(int segment)
+    {
+        for (var row = segment; row < _rows; row++)
+        {
+            _matrix[row, segment] = 0;
+        }
+        for (var col = segment; col < _cols; col++)
+        {
+            _matrix[segment, col] = 0;
+        }
+    }
+}
